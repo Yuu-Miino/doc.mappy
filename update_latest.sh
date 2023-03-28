63e7befa706cb62df1762d6a297edeefea62934f
Yuu Miino <ymiino@naruto-u.ac.jp>
2023-03-28 10:50:31 +0900

    Added `update_latest.sh`

---
+++ update_latest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+latest=$(
+    ls -d v* |
+    awk 'BEGIN { FS="."; } { f=substr($1, 2); s=$2; t=$3; print f, s, t, $0;}' |
+    sort -k 3n -k 2n -k 1n |
+    tail -1 |
+    awk '{print $4}'
+)
+ln -s $latest "latest"
